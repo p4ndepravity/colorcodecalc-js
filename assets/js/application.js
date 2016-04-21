ef9e35e84d86c2438360f19143bd9c93507b8855
Trent B <trent.bullard@gmail.com>
2016-04-21 09:22:59 -0500

    initial commit; implement bootstrap; implement add band tooltip; implement show bands function; implement add band function; implement remove band function without implement feature; implement CalculateOhmValue function; implement reset function;

---
+++ assets/js/application.js
@@ -0,0 +1,110 @@
+$(document).ready(function(){
+  $('.tooltip-class').tooltip({
+    selector: '[data-toggle=tooltip]',
+    container: 'body'
+  })
+  $('.band_btn').on('click', function(){
+    console.log($(this).text())
+    add_band($(this).text())
+  })
+})
+
+bands = []
+
+function band_btns(){
+  html_string = ''
+  for(band in static_bands){
+    if(bands.length<2){
+      if(band != 'gold' && band != 'silver'){
+        html_string += '<li><span class="btn btn-default btn-block band_btn" style="color: '+band+';">'+band+'</span></li>\n'
+      }
+    } else if(bands.length == 3){
+      if(band != 'black' && band != 'orange' && band != 'white'){
+        html_string += '<li><span class="btn btn-default btn-block band_btn" style="color: '+band+';">'+band+'</span></li>\n'
+      }
+    } else {
+      html_string += '<li><span class="btn btn-default btn-block band_btn" style="color: '+band+';">'+band+'</span></li>\n'
+    }
+  }
+  return html_string
+}
+
+function add_band_btn(){
+  if(bands.length<4){
+    html_string = '<span class="dropdown"><span class="tooltip-class dropdown-toggle" data-toggle="dropdown" id="add_band_btn" aria-haspopup="true"><button class="btn btn-default" data-toggle="tooltip" data-placement="top" data-original-title="Add Band">+</button></span><ul class="dropdown-menu" aria-labelledby="add_band_btn">'+band_btns()+'</ul></span>'
+    return html_string
+  } else {
+    return ''
+  }
+}
+
+function show_bands(){
+  html_string = ''
+  for(band in bands){
+    band = bands[band]
+    band_html = '<span class="tooltip-class"><span style="color: '+band.color+';" data-toggle="tooltip" data-placement="top" data-original-title="'+band.color+'">|</span></span>'
+    html_string += '<li>'+band_html+'</li>'
+  }
+  html_string = '<li><span class="glyphicon glyphicon-menu-left" aria-hidden="true"></span></li>'
+                +html_string+add_band_btn()+
+                '<li><span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span></li>'
+  $('#band-list').html(html_string)
+  return
+}
+
+function add_band(color){
+  bands.push(static_bands[color])
+  if(bands.length == 4){
+    CalculateOhmValue(bands[0].color, bands[1].color, bands[2].color, bands[3].color);
+  } else {
+    show_bands()
+  }
+  return
+}
+
+function remove_band(index){
+  bands.splice(index, 1)
+  show_bands()
+  if(bands.length == 4){
+    CalculateOhmValue(bands[0].color, bands[1].color, bands[2].color, bands[3].color)
+  } else {
+    show_bands()
+  }
+  return
+}
+
+function CalculateOhmValue(bandAColor, bandBColor, bandCColor, bandDColor){
+  resistance = 0
+  min_resistance = 0
+  max_resistance = 0
+
+  if(bandAColor){
+    a = static_bands[bandAColor]
+    resistance = a.sig_figs
+    if(bandBColor){
+      b = static_bands[bandBColor]
+      resistance = (resistance * 10) + b.sig_figs
+      if(bandCColor){
+        c = static_bands[bandCColor]
+        resistance = resistance * c.multiplier
+        if(bandDColor){
+          d = static_bands[bandDColor]
+          min_resistance = resistance * (1 - d.tolerance)
+          max_resistance = resistance * (1 + d.tolerance)
+        }
+      }
+    }
+  }
+
+  $('#idealResistance').val(resistance)
+  $('#minResistance').val(min_resistance)
+  $('#maxResistance').val(max_resistance)
+  show_bands()
+  return
+}
+
+function reset(){
+  bands = []
+  CalculateOhmValue()
+  return
+}
---
+++ assets/js/static_bands.js
@@ -0,0 +1,69 @@
+static_bands = {
+  black: {
+    color: 'black',
+    sig_figs: 0,
+    multiplier: 1,
+  },
+  brown: {
+    color: 'brown',
+    sig_figs: 1,
+    multiplier: 10,
+    tolerance: 0.01,
+  },
+  red: {
+    color: 'red',
+    sig_figs: 2,
+    multiplier: 100,
+    tolerance: 0.02,
+  },
+  orange: {
+    color: 'orange',
+    sig_figs: 3,
+    multiplier: 1000,
+  },
+  yellow: {
+    color: 'yellow',
+    sig_figs: 4,
+    multiplier: 10000,
+    tolerance: 0.05,
+  },
+  green: {
+    color: 'green',
+    sig_figs: 5,
+    multiplier: 100000,
+    tolerance: 0.005,
+  },
+  blue: {
+    color: 'blue',
+    sig_figs: 6,
+    multiplier: 1000000,
+    tolerance: 0.0025,
+  },
+  violet: {
+    color: 'violet',
+    sig_figs: 7,
+    multiplier: 10000000,
+    tolerance: 0.001,
+  },
+  gray: {
+    color: 'gray',
+    sig_figs: 8,
+    multiplier: 100000000,
+    tolerance: 0.0005,
+  },
+  white: {
+    color: 'white',
+    sig_figs: 9,
+    multiplier: 1000000000,
+  },
+  gold: {
+    color: 'gold',
+    multiplier: 0.1,
+    tolerance: 0.05,
+  },
+  silver: {
+    color: 'silver',
+    multiplier: 0.01,
+    tolerance: 0.1,
+  }
+}
